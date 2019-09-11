9e853faccb3fdf1880774569c2c98b325d5e573a
Jose Laverde <jf.laverde@uniandes.edu.co>
2019-09-10 23:17:55 -0500

    Prueba de concepto worker Cypress

---
+++ WorkerCypress/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace WorkerCypress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var psiNpmRunDist = new ProcessStartInfo
+            {
+                FileName = "cmd",
+                RedirectStandardInput = true,
+                //WorkingDirectory = guiProjectDirectory
+            };
+            var pNpmRunDist = Process.Start(psiNpmRunDist);
+            pNpmRunDist.StandardInput.WriteLine("npx cypress run");
+            pNpmRunDist.WaitForExit();
+            
+
+            
+
+        }
+    }
+}
