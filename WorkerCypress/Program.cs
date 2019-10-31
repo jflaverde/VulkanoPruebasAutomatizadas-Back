20b4a04dcac6152f274852dd1d546c08c252f930
Jose Laverde <jf.laverde@uniandes.edu.co>
2019-10-30 21:36:55 -0500

    general worker implemented

--- WorkerCypress/Program.cs
+++ WorkerCypress/Program.cs
@@ -1,20 +1,30 @@
-﻿using System;
+﻿using Data.DTO;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Diagnostics;
-using RabbitMQ.Client;
+using System.IO;
+using System.Linq;
 
 namespace WorkerCypress
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            TestCypress();
+            RabbitMQ rabbitMQ = new RabbitMQ();
 
+            var estrategia = rabbitMQ.GetMessages("QUEUE_E2E");
+
+            if(estrategia!=null)
+            {
+                TestCypress(estrategia);
+            }
         }
 
 
-        static void TestCypress()
+        static void TestCypress(EstrategiaDTO estrategia)
         {
             var psiNpmRunDist = new ProcessStartInfo
             {
@@ -25,10 +35,27 @@ namespace WorkerCypress
             var pNpmRunDist = Process.Start(psiNpmRunDist);
             pNpmRunDist.StandardInput.WriteLine("cd C:\\Windows\\System32\\cmd.exe");
 
-            pNpmRunDist.StandardInput.WriteLine("cd C:\\Users\\Sistemas\\Pruebas automatizadas\\Crypress Prestashop\\-201920_MISO4208\\scripts");
+            //Traer y copiar el archivo script al sitio donde están los scripts 
+            string rutaAbsoluta = "C:\\Users\\Sistemas\\source\\repos\\VulkanoPruebasAutomatizadas-Front\\VulkanoPruebasAutomatizadas-Front\\wwwroot";
+
+            string rutaScript = string.Concat(rutaAbsoluta,estrategia.TipoPruebas.First().Script.Script);
+
+            string rutaDestino = string.Concat("C:\\Users\\Sistemas\\Pruebas automatizadas\\E2E\\E2E\\cypress\\integration","\\",estrategia.TipoPruebas.First().Script.ID); 
+
+            if(!Directory.Exists(rutaDestino))
+            {
+                Directory.CreateDirectory(rutaDestino);
+            }
+
+            rutaDestino= string.Concat("C:\\Users\\Sistemas\\Pruebas automatizadas\\E2E\\E2E\\cypress\\integration", estrategia.TipoPruebas.First().Script.Script);
+
+            File.Copy(rutaScript, rutaDestino,true);
+
+            pNpmRunDist.StandardInput.WriteLine("cd C:\\Users\\Sistemas\\Pruebas automatizadas\\E2E\\E2E");
 
             pNpmRunDist.StandardInput.WriteLine("npx cypress run .");
             pNpmRunDist.WaitForExit();
+
         }
 
     }
