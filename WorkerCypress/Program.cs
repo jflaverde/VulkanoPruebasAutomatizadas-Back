b89533a47f5251f9893808f5c64a414b5098d8aa
Carlos Blanco <Takezo316@users.noreply.github.com>
2019-11-02 04:23:34 -0500

    Add files via upload

--- WorkerCypress/Program.cs
+++ WorkerCypress/Program.cs
@@ -1,7 +1,11 @@
 ﻿using Data.DTO;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
+using Controller;
 
 namespace WorkerWebsiteE2E
 {
@@ -22,43 +26,51 @@ namespace WorkerWebsiteE2E
 
         static void TestCypress(EstrategiaDTO estrategia)
         {
-            GeneralWorker.ActionsFile actionsFile = new GeneralWorker.ActionsFile();
-            var psiNpmRunDist = new ProcessStartInfo
+            foreach (TipoPruebaDTO tipoPrueba in estrategia.TipoPruebas)
             {
-                FileName = "cmd",
-                RedirectStandardInput = true,
-                //WorkingDirectory = guiProjectDirectory
-            };
-            var pNpmRunDist = Process.Start(psiNpmRunDist);
-            pNpmRunDist.StandardInput.WriteLine("cd C:\\Windows\\System32\\cmd.exe");
+                TipoPruebaController tipoPruebaController = new TipoPruebaController();
+                int idExecution = tipoPruebaController.InsertEjecucionTipoPrueba(estrategia.Estrategia_ID, estrategia.TipoPruebas[0].ID, 0, "");
 
-            //Traer y copiar el archivo script al sitio donde están los scripts 
-            //string rutaAbsoluta = "C:\\Users\\Sistemas\\source\\repos\\VulkanoPruebasAutomatizadas-Front\\VulkanoPruebasAutomatizadas-Front\\wwwroot";
-            string rutaAbsoluta = "D:\\Takezo316\\GitHub\\VulkanoPruebasAutomatizadas-Front\\VulkanoPruebasAutomatizadas-Front\\wwwroot\\uploads";
-            Directory.SetCurrentDirectory(@"D:\vulkanotest");
-            string cypath = Path.GetFullPath(@"D:");
-            string rutaScript = string.Concat(rutaAbsoluta, estrategia.TipoPruebas.First().Script.Script);
-            string rutaDestino = string.Concat(cypath, "\\", estrategia.TipoPruebas.First().Script.ID);
+                GeneralWorker.ActionsFile actionsFile = new GeneralWorker.ActionsFile();
+                var psiNpmRunDist = new ProcessStartInfo
+                {
+                    FileName = "cmd",
+                    RedirectStandardInput = true,
+                    //WorkingDirectory = guiProjectDirectory
+                };
+                var pNpmRunDist = Process.Start(psiNpmRunDist);
+                pNpmRunDist.StandardInput.WriteLine("cd C:\\Windows\\System32\\cmd.exe");
 
-            if (!Directory.Exists(rutaDestino))
-            {
-                Directory.CreateDirectory(rutaDestino);
+                //Traer y copiar el archivo script al sitio donde están los scripts 
+                //string rutaAbsoluta = "C:\\Users\\Sistemas\\source\\repos\\VulkanoPruebasAutomatizadas-Front\\VulkanoPruebasAutomatizadas-Front\\wwwroot";
+                string rutaAbsoluta = "D:\\Takezo316\\GitHub\\VulkanoPruebasAutomatizadas-Front\\VulkanoPruebasAutomatizadas-Front\\wwwroot\\uploads";
+                Directory.SetCurrentDirectory(@"D:\vulkanotest");
+                string cypath = Path.GetFullPath(@"D:");
+                string rutaScript = string.Concat(rutaAbsoluta, estrategia.TipoPruebas.First().Script.Script);
+                string rutaDestino = string.Concat(cypath, "\\", estrategia.TipoPruebas.First().Script.ID);
+
+                if (!Directory.Exists(rutaDestino))
+                {
+                    Directory.CreateDirectory(rutaDestino);
+                }
+
+                //rutaDestino= string.Concat("C:\\Users\\Sistemas\\Pruebas automatizadas\\E2E\\E2E\\cypress\\integration", estrategia.TipoPruebas.First().Script.Script);
+                rutaDestino = string.Concat(cypath, estrategia.TipoPruebas.First().Script.Script);
+
+                string filename = Path.GetFileNameWithoutExtension(rutaDestino);
+
+                File.Copy(rutaScript, rutaDestino, true);
+
+                actionsFile.UnzipFile(rutaScript, string.Concat(cypath, "\\", estrategia.TipoPruebas.First().Script.ID, "\\"));
+
+                pNpmRunDist.StandardInput.WriteLine(string.Concat("cd ", cypath, "\\", estrategia.TipoPruebas.First().Script.ID, "\\", filename));
+                pNpmRunDist.StandardInput.WriteLine("npm i");
+                pNpmRunDist.StandardInput.WriteLine("npx cypress run .");
+                pNpmRunDist.WaitForExit();
+
+                tipoPruebaController.InsertEjecucionTipoPrueba(estrategia.Estrategia_ID, estrategia.TipoPruebas[0].ID, idExecution, "");
+
             }
-
-            //rutaDestino= string.Concat("C:\\Users\\Sistemas\\Pruebas automatizadas\\E2E\\E2E\\cypress\\integration", estrategia.TipoPruebas.First().Script.Script);
-            rutaDestino = string.Concat(cypath, estrategia.TipoPruebas.First().Script.Script);
-            
-            string filename = Path.GetFileNameWithoutExtension(rutaDestino);
-
-            File.Copy(rutaScript, rutaDestino, true);
-
-            actionsFile.UnzipFile(rutaScript, string.Concat(cypath, "\\", estrategia.TipoPruebas.First().Script.ID, "\\"));
-
-            pNpmRunDist.StandardInput.WriteLine(string.Concat("cd ", cypath,"\\", estrategia.TipoPruebas.First().Script.ID, "\\", filename));
-            pNpmRunDist.StandardInput.WriteLine("npm i");
-            pNpmRunDist.StandardInput.WriteLine("npx cypress run .");
-            pNpmRunDist.WaitForExit();
-
         }
 
     }
