8eef45453489ebed988fda20659228cb3cb8caaf
Jose Laverde <jf.laverde@uniandes.edu.co>
2019-11-16 19:42:10 -0500

    Se cambia de net core a framework todos los proyectos

---
+++ DataFramework/DTO/TipoPruebaDTO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.DTO
+{
+    public class TipoPruebaDTO
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Parametros { get; set; }
+        public ScriptDTO Script { get; set; }
+        public string Descripcion { get; set; }
+        public EstadoDTO Estado { get; set; }
+        public MQTipoPruebaDTO MQTipoPrueba { get; set; }
+        public List<HistorialEjecucionPruebaDTO> HistorialEjecuciones { get; set; }
+        public int CantidadEjecuciones { get; set; }
+        public DateTime TiempoEjecucion { get; set; }
+        public string Semilla { get; set; }
+
+        public TipoPruebaDTO() {
+            HistorialEjecuciones = new List<HistorialEjecucionPruebaDTO>();
+            MQTipoPrueba = new MQTipoPruebaDTO();
+            Estado = new EstadoDTO();
+            Script = new ScriptDTO();
+        }
+    }
+}
